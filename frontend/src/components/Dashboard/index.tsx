3adae0fa078aa5d39c8668c9b187e3894348095c
param pam pam <arifamjy@gmail.com>
2023-03-06 13:33:56 +0700

    ui: little tweak to the stick nav styling

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 const Dashboard = () => (
-  <main className="w-full overflow-auto">
+  <main className="ml-[65px] w-full xl:ml-[250px]">
     <div className="h-[300vh]">Dashboard</div>
   </main>
 )
--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -2,15 +2,16 @@ import TwitterIcon from '../Icons/TwitterIcon'
 import Navigations from './Navigations'
 
 const Sidebar = () => (
-  <header className="flex h-screen flex-col items-center justify-between border-r p-3 xl:w-[250px]">
+  <header className="fixed flex h-screen flex-col items-center justify-between border-r p-3 xl:w-[250px]">
     <section className="flex flex-col gap-8">
       <h1>
         <TwitterIcon />
       </h1>
       <Navigations />
     </section>
-    <section className="mb-2 xl:self-start">
+    <section className="mb-2 flex items-center gap-4">
       <div className="h-[40px] w-[40px] rounded-full bg-slate-500" />
+      <p className="hidden xl:block">user blablabla</p>
     </section>
   </header>
 )
