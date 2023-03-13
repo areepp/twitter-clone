5f9864b239418b355f9c27cad823e8f092ab6e47
param pam pam <arifamjy@gmail.com>
2023-03-13 11:51:55 +0700

    Revert "feat: setup supertokens"
    This reverts commit 588352dbbb9f15026743e9d4c1125ef6673db2ab.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,10 @@ import * as dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import supertokens from 'supertokens-node'
-import {
-  middleware as supertokensMiddleware,
-  errorHandler as supertokensErrorHandler,
-} from 'supertokens-node/framework/express'
 import credentials from './middlewares/credentials'
 import errorHandler from './middlewares/errorHandler'
-import initSuperTokens from './utils/initSuperTokens'
 
 dotenv.config()
-
-initSuperTokens()
 
 const app = express()
 
@@ -25,17 +17,13 @@ app.use(
         ? 'http://localhost:3000'
         : 'https://your-domain',
     credentials: true,
-    allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
   }),
 )
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(supertokensMiddleware()) // supertokens middlewares
-
 app.get('/', (_req, res) => res.send('Hello world!'))
 
-app.use(supertokensErrorHandler())
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
