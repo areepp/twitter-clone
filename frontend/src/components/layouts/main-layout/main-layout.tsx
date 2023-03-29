a0811dbb51b72d8f7520403808d40a27aeb34c6f
param pam pam <arifamjy@gmail.com>
2023-03-29 11:10:15 +0700

    style(nav): make the nav wider

--- frontend/src/components/layouts/main-layout/main-layout.tsx
+++ frontend/src/components/layouts/main-layout/main-layout.tsx
@@ -4,7 +4,7 @@ import { SideNav } from '@/components/side-nav'
 export const MainLayout = ({ children }: { children: React.ReactNode }) => (
   <div className="container mx-auto flex h-screen xl:px-20">
     <SideNav />
-    <main className="ml-[65px] max-w-[600px] grow border-r xl:ml-[250px]">
+    <main className="ml-[72px] max-w-[600px] grow border-r xl:ml-[260px]">
       {children}
     </main>
     <RightSection />
--- frontend/src/components/side-nav/side-nav.tsx
+++ frontend/src/components/side-nav/side-nav.tsx
@@ -1,4 +1,5 @@
 import { useGetUserQueryData } from '@/features/auth'
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import { TwitterIcon } from '../elements'
 import Logout from './logout'
@@ -8,7 +9,7 @@ export const SideNav = () => {
   const user = useGetUserQueryData()
 
   return (
-    <header className="fixed flex h-screen flex-col items-center justify-between border-r p-3 xl:w-[250px]">
+    <nav className="fixed flex h-screen w-[72px] flex-col items-center justify-between border-r p-3 pb-5 xl:w-[260px] xl:items-start">
       <section className="flex flex-col gap-8">
         <h1>
           <TwitterIcon />
@@ -17,17 +18,23 @@ export const SideNav = () => {
         {user && <Logout />}
       </section>
       {user && (
-        <section className="mb-2 flex items-center gap-4">
-          <Image
-            src={user.profilePicture ?? '/twitter-default-pp.png'}
-            width="40"
-            height="40"
-            className="rounded-full"
-            alt="photo profile"
-          />
-          <p className="hidden xl:block">user blablabla</p>
+        <section className="flex items-center justify-between rounded-full p-3 hover:bg-gray-200 xl:w-full">
+          <div className="flex items-center gap-3">
+            <Image
+              src={user.profilePicture ?? '/twitter-default-pp.png'}
+              width="40"
+              height="40"
+              className="rounded-full"
+              alt="photo profile"
+            />
+            <div className="hidden text-sm xl:block">
+              <p>{user.username}</p>
+              <p className="text-dark-gray">@{user.username}</p>
+            </div>
+          </div>
+          <EllipsisHorizontalIcon className="hidden h-5 w-5 xl:block" />
         </section>
       )}
-    </header>
+    </nav>
   )
 }
