7e20871062290a3734acbc383e84ff60e76c87bc
Arif Amijoyo <arifamjy@gmail.com>
2023-07-15 10:23:52 +0700

    fix: prisma seed error

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,10 +3,10 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  for (let i = 0; i < 15; i++) {
+  for (let i = 0; i < 20; i++) {
     await prisma.tweet.create({
       data: {
-        text: 'tweet created by seed d',
+        text: 'tweet created by seed b',
         author: {
           connect: {
             id: '117021340127220940452',
@@ -25,6 +25,3 @@ main()
     console.error(e)
     await prisma.$disconnect()
   })
-
-// "8ecd62fe-6233-4ba6-a4c5-95a17576faff"
-// "117021340127220940452"
